f013d3406223565e65536693c0ead13e70ebdaee
sabbir2-dot <sabbirahmed760681@gmail.com>
2024-12-29 13:15:14 +0000

    files moved from core app to authentication app

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import UserProfile, Book, BorrowHistory, BookReview
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Book)
+admin.site.register(BorrowHistory)
+admin.site.register(BookReview)
