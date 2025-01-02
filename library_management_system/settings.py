b30a1841ecb844417d7b3466bce78ed488ca9b47
sabbir2-dot <sabbirahmed760681@gmail.com>
2025-01-02 23:49:03 +0000

    set up allowed host again added

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://librarymanagementsystem-qd8f.onrender.com', 'https://*.127.0.0.1']
 
 # Application definition
 
